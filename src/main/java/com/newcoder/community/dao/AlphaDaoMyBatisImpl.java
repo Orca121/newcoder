5dfcb45c730af5f87567d07ba74ef738539b3e79
Orca121 <Hibernian_Druid@126.com>
2022-09-21 22:50:27 +0800

    20220921,完成前七章

--- src/main/java/com/newcoder/community/dao/AlphaDaoMyBatisImpl.java
+++ src/main/java/com/newcoder/community/dao/AlphaDaoMyBatisImpl.java
@@ -1,13 +1,13 @@
-package com.newcoder.community.dao;
-
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Repository;
-
-@Repository
-@Primary
-public class AlphaDaoMyBatisImpl implements AlphaDao{
-    @Override
-    public String select() {
-        return "MyBatis";
-    }
-}
+//package com.newcoder.community.dao;
+//
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//@Primary
+//public class AlphaDaoMyBatisImpl implements AlphaDao{
+//    @Override
+//    public String select() {
+//        return "MyBatis";
+//    }
+//}
